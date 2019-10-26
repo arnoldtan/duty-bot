8c416c71ac1d759d2daa7eb6b8eecf2f1a6ef15d
Arnold Tan <tanatarnold@gmail.com>
2019-10-26 15:08:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = require('token').TOKEN;
+const token = require('./token.js').TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
